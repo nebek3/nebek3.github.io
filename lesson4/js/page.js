9d1dc2706f495b460fbb0ddc4bf4da3c222a5a87
nebek3 <neb14003@byui.edu>
2020-10-10 13:42:52 -0600

    update

--- lesson4/js/page.js
+++ lesson4/js/page.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',(event)=>{
+/*window.addEventListener('load',(event)=>{
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.menu')          
 
@@ -6,4 +6,8 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-});
+});*/
+
+function changeMenu(){
+    document.getElementsByClassName("menu")[0].classList.toggle("responsive")
+}
