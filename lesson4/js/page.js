06a1be4bc08ae492399bde42f2c0bcfbe80ef2b0
nebek3 <neb14003@byui.edu>
2020-10-17 12:52:09 -0600

    wk5 update

--- lesson4/js/page.js
+++ lesson4/js/page.js
@@ -11,3 +11,10 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 function toggleMenu(){
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
 }
+window.addEventListener('load',(event)=>{
+    const lu = document.querySelector('#lastupdated');
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector('#copyrightyear');
+    cry.textContent = new Date().getFullYear();
+})
